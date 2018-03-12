1377ee9e132466b7c175806021105e9ef3512fa0
faizal <faizalramanda@gmail.com>
2018-03-12 20:50:24 +0700

    done faizalrmnd

--- calculator.js
+++ calculator.js
@@ -2,21 +2,62 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num) {
+    this.number = num
   }
-  add () {
+  add (num) {
+    this.number += num;
+    return this
   }
-  substract () {
+  substract (num) {
+    this.number -= num;
+    return this
   }
-  multiply () {
+  multiply (num) {
+    this.number *= num;
+    return this
   }
-  divide () {
+  divide (num) {
+    this.number /= num;
+    return this
   }
-  square () {
+  square (power) {
+    let number = this.number
+    for (let i = 0; i < power; i++) {
+      this.number *= number
+    }
+    return this
   }
   squareRoot () {
+    // for (var i = 0; i < this.number; i++) {
+    //   if (i * i === this.number) {
+    //     this.number = i
+    //     return this
+    //   }
+    // }
+    // var sqrt = this.number
+    this.number = Math.sqrt(this.number)
+    return this
+  }
+  circleArea(radius) {
+    const pi = 22/7
+    this.number = pi * (radius*radius)
+    return this
+  }
+  circleCirc(radius) {
+    const po = 22/7
+    this.number = 2 * pi * radius
+    return this
+  }
+
+  show() {
+    return this.number
   }
 }
+
+var calculator = new Calculator(9)
+
+console.log(calculator.squareRoot());
 
 /** note : you can use several features from ecmascript, such as:
 * - Classes
