974c3f3f836d6a4e019ab871538a793f26db5cc9
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-03-12 15:38:43 +0700

    calc + - / * akar pangkat lingkaran

--- calculator.js
+++ calculator.js
@@ -4,20 +4,51 @@ class Calculator {
   //write your code here
   constructor () {
   }
-  add () {
+
+  initNumber(num){
+    this.number = num;
+    return this
   }
-  substract () {
+
+
+  add (addnum) {
+  this.number += addnum
+
+  return this
   }
-  multiply () {
+
+  substract (substractnum) {
+    this.number -= substractnum
+    return this
   }
-  divide () {
+  multiply (mulNum) {
+    this.number *= mulNum
+    return this
   }
-  square () {
+  divide (divNum) {
+    this.number  /= divNum
+    return this
   }
-  squareRoot () {
+  square (sqNum) {
+    this.number = Math.pow(this.number, sqNum)
+    return this
+  }
+  squareRoot (sqrNum) {
+    this.number = sqrNum
+    this.number = Math.sqrt(this.number)
+    return this
+  }
+  circleArea(cirNum){
+    this.number = cirNum
+    if (this.number % 7 === 0) {
+      this.number = (22 / 7) * this.number * this.number;
+    } else {
+      this.number = 3.14 * this.number * this.number;
+    }
+    return this
   }
 }
-
+//var hitung = new Calculator()
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters
@@ -26,6 +57,11 @@ class Calculator {
 * - Method Chaining
 */
 
-module.exports = {
-  Calculator
+//console.log(hitung.add(10));
+
+
+
+
+ module.exports = {
+   Calculator
 }
--- main.js
+++ main.js
@@ -3,3 +3,11 @@
 const {Calculator} = require("./calculator.js")
 
 // execute function on calculator.js in here
+var hitung = new Calculator()
+console.log(hitung.initNumber(10).add(2));
+console.log(hitung.initNumber(20).substract(16));
+console.log(hitung.initNumber(20).multiply(16));
+console.log(hitung.initNumber(20).divide(4));
+console.log(hitung.initNumber(20).square(2));
+console.log(hitung.squareRoot(100));
+console.log(hitung.circleArea(7));
